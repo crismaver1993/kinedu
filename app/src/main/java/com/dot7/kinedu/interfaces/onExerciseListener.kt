bedaf3f09a955b59154ab11e67376fa05f2a9416
BRM11957 <cristofer.marin@banregio.com>
2019-12-01 22:16:19 -0600

    feature:  Se agrega interfaz para manipular información

---
+++ app/src/main/java/com/dot7/kinedu/interfaces/onExerciseListener.kt
@@ -0,0 +1,8 @@
+package com.dot7.kinedu.interfaces
+
+import com.dot7.kinedu.models.ActivityDataInfo
+
+interface onExerciseListener {
+
+    fun showActivityDetail( activityInfo: ActivityDataInfo)
+}

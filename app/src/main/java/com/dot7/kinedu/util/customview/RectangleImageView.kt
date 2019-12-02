92b53a61e1ed1d6e969d15150c9c8796df4fbf4b
BRM11957 <cristofer.marin@banregio.com>
2019-12-02 06:44:10 -0600

    feature:  Se agregan customviews

--- app/src/main/java/com/dot7/kinedu/util/customview/RectangleImageView.kt
+++ app/src/main/java/com/dot7/kinedu/util/customview/RectangleImageView.kt
@@ -1,18 +1,21 @@
-package com.dot7.kinedu.util
+package com.dot7.kinedu.util.customview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 
-class SquareImageview : AppCompatImageView {
+class RectangleImageView : AppCompatImageView {
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+        val width = View.MeasureSpec.getSize(widthMeasureSpec)
+        val desireheight = width * 2 / 3
+        super.onMeasure(widthMeasureSpec, View.MeasureSpec.makeMeasureSpec(desireheight, View.MeasureSpec.EXACTLY))
     }
+
 }
---
+++ app/src/main/java/com/dot7/kinedu/util/customview/SquareImageView.kt
@@ -0,0 +1,18 @@
+package com.dot7.kinedu.util.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class SquareImageview : AppCompatImageView {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}
